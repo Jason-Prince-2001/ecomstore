322ec72155da96ee99a61210aee1ef0a960558be
Jason-Prince-2001 <princeijason@gmail.com>
2026-05-21 13:00:23 +0530

    rename products file

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import products from "../../data/products.js";
+import products from "../../data/temp.js";
 
 const productSlice = createSlice({
   name: 'products',
